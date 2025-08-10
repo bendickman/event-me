56a0308ddf6984fd3bfcb6a2b9d78b6a1dc8b167
bendickman <ben.dickman@gmail.com>
2025-08-10 17:36:13 +0100

    client-side error handling

--- Source/event-me-client-app/src/app/router/Routes.tsx
+++ Source/event-me-client-app/src/app/router/Routes.tsx
@@ -1,10 +1,13 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import App from "../layout/App";
 import HomePage from "../../features/home/HomePage";
 import EventDashboard from "../../features/events/dashboard/EventDashboard";
 import EventForm from "../../features/events/form/EventForm";
 import EventDetails from "../../features/events/details/EventDetails";
 import Counter from "../../features/counter/Counter";
+import TestErrors from "../../features/error/TestErrors";
+import NotFound from "../../features/error/NotFound";
+import ServerError from "../../features/error/ServerError";
 
 const router = createBrowserRouter([
     {
@@ -17,6 +20,10 @@ const router = createBrowserRouter([
             { path: '/create-event', element: <EventForm key='create' /> },
             { path: '/edit-event/:id', element: <EventForm /> },
             { path: '/counter', element: <Counter /> },
+            { path: '/errors', element: <TestErrors /> },
+            { path: 'not-found', element: <NotFound /> },
+            { path: 'server-error', element: <ServerError /> },
+            { path: '*', element: <Navigate replace to='/not-found' /> },
         ]
     }
 ])
